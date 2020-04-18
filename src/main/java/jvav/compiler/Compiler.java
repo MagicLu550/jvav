6caf0fe1c3d209f16d128bed32c09865b0bfd54e
magiclu550 <843983728@qq.com>
2020-04-18 13:52:28 +0800

    del

--- src/main/java/jvav/compiler/Compiler.java
+++ src/main/java/jvav/compiler/Compiler.java
@@ -1,11 +1,6 @@
 package jvav.compiler;
 
-import java.io.IOException;
-
 public class Compiler {
 
-    public static void main(String[] args) throws IOException {
-        JvavReader reader = new JvavReader();
-        System.out.println(reader.readJvav("test.jvav"));
-    }
+
 }
---
+++ src/main/java/jvav/compiler/Runner.java
@@ -0,0 +1,8 @@
+package jvav.compiler;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        
+    }
+}
